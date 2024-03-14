8765b56c67a3c170d3a3e59c01fb2fab0f0a1807
sma66-tmw <sma66@tmw.com>
2024-03-14 17:41:37 +0530

    add two dropdown fields for filtering news

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,32 @@ import newsData from "/mockData";
 export default function Home() {
   return (
     <div className="container px-3 is-max-desktop mb-5">
+    <div className="mb-5 is-justify-content-space-between is-flex">
+      <div className="field">
+        <label className="label">Topic</label>
+        <div className="control">
+          <div className="select is-rounded">
+            <select>
+                <option value="ENVIRONMENT">ENVIRONMENT</option>
+                <option value="FINANCE">FINANCE</option>
+                <option value="SPORTS">SPORTS</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">News Source</label>
+        <div className="control">
+          <div className="select is-rounded">
+            <select>
+                <option value="CNN">CNN</option>
+                <option value="BBC">BBC</option>
+                <option value="FOX">FOX</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
       <div className="news-list columns is-flex-wrap-wrap is-justify-content-space-between">
         {newsData.news.map((newsItem) => (
           <Card key={newsItem.id} {...newsItem} />
