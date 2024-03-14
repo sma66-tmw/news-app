508b63793b27f603f5b4d9e597a9a38cca8e137f
sma66-tmw <sma66@tmw.com>
2024-03-14 13:16:05 +0530

    rendering react into html

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function App() {
+  return <h1>React app rendered</h1>;
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("News app loaded");
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
