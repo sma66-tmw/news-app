0450d99f9afb11223b26f694f115c45920bee06f
sma66-tmw <sma66@tmw.com>
2024-03-16 23:11:01 +0530

    add router to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import Home from "./pages/Home"
+import Router from "./Router";
 
 function App() {
   return (
-    <Home />
+    <Router />
   );
 }
 export default App;
