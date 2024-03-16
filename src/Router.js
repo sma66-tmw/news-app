935dbbc69102e424a88f2a42bbda9178c912dc1a
sma66-tmw <sma66@tmw.com>
2024-03-16 23:13:18 +0530

    error path

--- src/Router.js
+++ src/Router.js
@@ -4,11 +4,15 @@ import {
   } from "react-router-dom";
   import Home from "./pages/Home";
   
+  function ErrorElement() {
+    return <h1 className="title m-5 has-text-centered">404 Page Not Found</h1>
+  }
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+      errorElement: <ErrorElement />
     },
   ]);
 
